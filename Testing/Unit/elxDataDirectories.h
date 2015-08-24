148cc3c51d2ae7e908331c80ddbd4d28a17ec447
Kasper Marstal <kaspermarstal@gmail.com>
2015-08-24 15:29:12 +0200

    COMP: ELASTIX-1 Deleted auto generated file that was accidentally commited

--- Testing/Unit/elxDataDirectories.h
+++
@@ -1,8 +0,0 @@
-#ifndef __elxTestDataDirectories_h
-#define __elxTestDataDirectories_h
-
-#define ELASTIX_UNITTEST_INPUT_DATA_DIR "/Users/kasper/Development/build/SuperElastix/Elastix-build/Testing/Unit/Data/Input"
-#define ELASTIX_UNITTEST_OUTPUT_DATA_DIR "/Users/kasper/Development/build/SuperElastix/Elastix-build/Testing/Unit/Data/Output"
-#define ELASTIX_UNITTEST_BASELINE_DATA_DIR "/Users/kasper/Development/build/SuperElastix/Elastix-build/Testing/Unit/Data/Baseline"
-
-#endif // __elxTestDataDirectories_h
