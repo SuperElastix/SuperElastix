3f58f6169c1fd972c49099bb452c15842adaf96d
Floris Berendsen <floris.berendsen@gmail.com>
2015-10-07 10:33:11 +0200

    WIP: getting factory in the main project. BUG: typo elxITKRequiredModules vs elxRequiredITKModules

--- Modules/Core/elxModuleCore.cmake
+++ Modules/Core/elxModuleCore.cmake
@@ -1,11 +1,13 @@
 set( ${MODULE}_INCLUDE_DIRS
   ${CMAKE_SOURCE_DIR}/${MODULE_PATH}/Common/include
   ${CMAKE_SOURCE_DIR}/${MODULE_PATH}/Blueprints/include
+  ${CMAKE_SOURCE_DIR}/${MODULE_PATH}/Install/include
 )
 
 set( ${MODULE}_SOURCE_FILES
   ${CMAKE_SOURCE_DIR}/${MODULE_PATH}/Blueprints/src/elxComponentDescriptor.cxx
   ${CMAKE_SOURCE_DIR}/${MODULE_PATH}/Blueprints/src/elxBlueprint.cxx
+  ${CMAKE_SOURCE_DIR}/${MODULE_PATH}/Blueprints/src/itkFactory.cxx
 )
 
 set( ${MODULE}_LIBRARIES 
