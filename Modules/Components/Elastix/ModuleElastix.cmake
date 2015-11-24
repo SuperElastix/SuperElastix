d4d774faaa61f2de118f4a960f5dfd5071d4ce9d
Kasper Marstal <kaspermarstal@gmail.com>
2015-11-24 16:49:31 +0100

    ENH: Add USE_SYSTEM_ELASTIX option

--- Modules/Components/Elastix/ModuleElastix.cmake
+++ Modules/Components/Elastix/ModuleElastix.cmake
@@ -2,6 +2,7 @@
 set( MODULE ModuleElastix )
 
 if( NOT EXISTS ${ELASTIX_USE_FILE} )
+  set( ELASTIX_USE_FILE )
   message( FATAL_ERROR "${MODULE} could not find ELASTIX_USE_FILE. Use the SuperBuild or manually point the ELASTIX_USE_FILE CMake variable to the UseElastix.cmake file in the root of your elastix build tree." )
 endif()
 
