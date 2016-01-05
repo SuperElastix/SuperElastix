17a138cbcc4595033e73e63d05c70546d7362834
Kasper Marstal <kaspermarstal@gmail.com>
2016-01-05 20:00:27 +0100

    ENH: Move ElastixFilter and TransformixFilter upstream

--- Modules/Core/ModuleCore.cmake
+++ Modules/Core/ModuleCore.cmake
@@ -13,8 +13,6 @@ file(GLOB ${MODULE}_HEADER_FILES "${${MODULE}_SOURCE_DIR}/*/include/*.*")
 #@Kasper: file GLOB is generally disencouraged for source files (missing files are not detected and CMake doesn't know if the project has to be updated when due to a checkout extra source files are in the tree), but since this I think is not really an issue for header files...
 #I read: CMake will figure out that they're headers; it won't try to build them. http://stackoverflow.com/questions/8316104/specify-how-cmake-creates-visual-studio-project
 
-
-
 # Export libraries
 set( ${MODULE}_LIBRARIES 
   ${MODULE}
@@ -23,7 +21,6 @@ set( ${MODULE}_LIBRARIES
 # Module source files
 set( ${MODULE}_SOURCE_FILES
   ${${MODULE}_SOURCE_DIR}/Blueprints/src/elxBlueprint.cxx
-  ${${MODULE}_SOURCE_DIR}/ParameterObject/src/elxParameterObject.cxx
   ${${MODULE}_SOURCE_DIR}/ComponentInterface/src/ComponentBase.cxx
   ${${MODULE}_SOURCE_DIR}/ExampleComponents/src/Example3rdPartyCode.cxx
   ${${MODULE}_SOURCE_DIR}/ExampleComponents/src/Example4thPartyCode.cxx
@@ -38,7 +35,5 @@ set( ${MODULE}_SOURCE_FILES
 )
 
 # Compile library
-
-add_library( ${MODULE} STATIC "${${MODULE}_SOURCE_FILES}" ${${MODULE}_HEADER_FILES})
-
+add_library( ${MODULE} STATIC "${${MODULE}_SOURCE_FILES}" ${${MODULE}_HEADER_FILES} )
 target_link_libraries( ${MODULE} ${ELASTIX_LIBRARIES} )
