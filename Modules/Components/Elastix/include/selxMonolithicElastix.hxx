9c85d908f85b968058793991662dd172533d0519
Floris Berendsen <floris.berendsen@gmail.com>
2016-10-03 14:05:33 +0200

    ENH: refactored ElastixComponents to use TemplateProperties() in MeetsCriterion()

--- Modules/Components/Elastix/include/selxMonolithicElastix.hxx
+++ Modules/Components/Elastix/include/selxMonolithicElastix.hxx
@@ -94,42 +94,19 @@ bool
 MonolithicElastixComponent< Dimensionality, TPixel >
 ::MeetsCriterion( const CriterionType & criterion )
 {
-  bool hasUndefinedCriteria( false );
-  bool meetsCriteria( false );
-  if( criterion.first == "ComponentProperty" )
+  bool hasUndefinedCriteria(false);
+  bool meetsCriteria(false);
+  auto status = CheckTemplateProperties(this->TemplateProperties(), criterion);
+  if (status == CriterionStatus::Satisfied)
   {
-    meetsCriteria = true;
-    for( auto const & criterionValue : criterion.second )  // auto&& preferred?
-    {
-      if( criterionValue != "SomeProperty" )   // e.g. "GradientDescent", "SupportsSparseSamples
-      {
-        meetsCriteria = false;
-      }
-    }
+    return true;
   }
-  else if( criterion.first == "Dimensionality" )  //Supports this?
+  else if (status == CriterionStatus::Failed)
   {
-    meetsCriteria = true;
-    for( auto const & criterionValue : criterion.second )  // auto&& preferred?
-    {
-      if( std::stoi( criterionValue ) != Dimensionality )
-      {
-        meetsCriteria = false;
-      }
-    }
-  }
-  else if( criterion.first == "PixelType" )  //Supports this?
-  {
-    meetsCriteria = true;
-    for( auto const & criterionValue : criterion.second )  // auto&& preferred?
-    {
-      if( criterionValue != Self::GetPixelTypeNameString() )
-      {
-        meetsCriteria = false;
-      }
-    }
-  }
-  else if( criterion.first == "RegistrationPreset" )  //Supports this?
+    return false;
+  } // else: CriterionStatus::Unknown
+
+  else if (criterion.first == "RegistrationPreset")  //Supports this?
   {
     // Temporary solution: RegistrationPreset: rigid, nonrigid, etc overwrite the current selxparameterObject.
     // Warning: the order of Criteria matters, since selxparameterObject may be overwritten
