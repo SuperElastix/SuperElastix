5e54b9a631a34ff3074ded9d3badf736785f2d34
Niels Dekker <N.Dekker@lumc.nl>
2017-06-13 17:09:19 +0200

    ENH: Added unit tests for our ITK/nifti name mangling patch (which solved issue #50, "Conflict between ITK's nifti and NiftyReg's nifti library"). Note: These unit tests are facilitated by Kasper's fix of issue #57 (Support multiple CXX files in a test project).

---
+++ Modules/Components/Niftyreg/test/selxNiftiFunctionPtrs.h
@@ -0,0 +1,38 @@
+#ifndef selxCommonNiftiFunctionTypes_h
+#define selxCommonNiftiFunctionTypes_h
+
+/*=========================================================================
+*
+*  Copyright Leiden University Medical Center, Erasmus University Medical
+*  Center and contributors
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+
+#include <stdlib.h>  // For size_t.
+
+// Meant to store the pointers to a few functions common to the ITK version and the Niftyreg version of nifti, 
+// that are included with the SuperBuild of SuperElastix.
+// (The list is not meant to be complete.)
+
+namespace selx
+{
+	struct NiftiFunctionPtrs
+	{
+		int(*get_filesize_FunctionPtr)(const char *);
+		void(*swap_2bytes_FunctionPtr)(size_t, void *);
+	};
+}
+
+#endif
