69bf018945163559094a4ac649b3cfb3c8d4bb18
Floris Berendsen <floris.berendsen@gmail.com>
2017-12-08 08:27:02 +0100

    ENH: CompiledLibraryComponents is generated by Cmake based on USE_${Module} flags
    TODO: separate componentgroups from modules

--- Modules/Filter/CompiledLibraryComponents.cmake
+++ Modules/Filter/CompiledLibraryComponents.cmake
@@ -3,8 +3,21 @@
 # in the bin directory. This file defines the components to be compiled.
 #
 
-set( MODULE_COMPONENT_GROUP_INCLUDE_FILES "#include \"selxModuleSinksAndSources.h\"" )
-set( MODULE_COMPONENT_TYPELISTS "ModuleSinksAndSourcesComponents" )
+set( MODULE_COMPONENT_GROUP_INCLUDE_FILES ) 
+set( MODULE_COMPONENT_TYPELISTS_ )
+
+foreach( MODULE ${SUPERELASTIX_MODULES} )
+  if( ${USE_${MODULE}} )
+    set( MODULE_COMPONENT_GROUP_INCLUDE_FILES "${MODULE_COMPONENT_GROUP_INCLUDE_FILES}#include \"selx${MODULE}.h\"\n" )
+    set( MODULE_COMPONENT_TYPELISTS_ "${MODULE_COMPONENT_TYPELISTS_}  ${MODULE}Components,\n")
+  endif()
+endforeach()
+
+string(LENGTH ${MODULE_COMPONENT_TYPELISTS_} LEN_WITH_COMMA)
+math(EXPR LEN_WITHOUT_COMMA "${LEN_WITH_COMMA}-2" ) 
+message( STATUS  ${LEN_WITHOUT_COMMA} )
+string(SUBSTRING ${MODULE_COMPONENT_TYPELISTS_} 0 ${LEN_WITHOUT_COMMA} MODULE_COMPONENT_TYPELISTS)
+unset(LEN)
 
 configure_file(
   ${${MODULE}_SOURCE_DIR}/include/selxCompiledLibraryComponents.h.in
