3e714d5bf53c2017243aa6928c99a30d06c87ea6
Niels Dekker <N.Dekker@lumc.nl>
2018-01-19 15:36:23 +0100

    WIP: Hopefully fixed "fatal: Not a git repository (or any of the parent directories): .git" at https://stingray.researchlumc.nl/job/SuperElastix/job/SuperElastix/job/SELX-126-Make-compilation-logs-public/24/console (3rd attempt)

--- CTest.cmake
+++ CTest.cmake
@@ -17,9 +17,9 @@
 #
 #=========================================================================
 
-# Directories relative to the current directory (either src or build).
-set(CTEST_SOURCE_DIRECTORY "../src")
-set(CTEST_BINARY_DIRECTORY "../build")
+# Directories relative to the current directory.
+set(CTEST_SOURCE_DIRECTORY "src")
+set(CTEST_BINARY_DIRECTORY "build")
 
 set(CTEST_SITE "lkeb-selx01")
 
