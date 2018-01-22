0cd9a72d3d3b6fe19f01271d8c1bdd2f3be802b2
Niels Dekker <N.Dekker@lumc.nl>
2018-01-22 16:14:15 +0100

    WIP: Added copy of CTest.cmake to SuperElastix subdirectory. Suggested by Floris.

---
+++ SuperBuild/CTest.cmake
@@ -0,0 +1,54 @@
+#=========================================================================
+#
+#  Copyright Leiden University Medical Center, Erasmus University Medical 
+#  Center and contributors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#=========================================================================
+
+# Directories relative to the current directory.
+set(CTEST_SOURCE_DIRECTORY "src")
+set(CTEST_BINARY_DIRECTORY "build")
+
+set(CTEST_SITE "lkeb-selx01")
+
+find_package( Git REQUIRED )
+
+execute_process (
+    COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+    OUTPUT_VARIABLE SELX_GIT_COMMIT_SHA 
+)
+execute_process (
+    COMMAND ${GIT_EXECUTABLE} name-rev --name-only HEAD
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
+    OUTPUT_VARIABLE SELX_GIT_BRANCH_NAME 
+)
+
+set(CTEST_BUILD_NAME "${SELX_GIT_BRANCH_NAME};commit=SHA\\:${SELX_GIT_COMMIT_SHA}")
+
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_OPTIONS "-j4")
+
+set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION} --build ${CTEST_BINARY_DIRECTORY}")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DWITH_TESTING:BOOL=ON ${CTEST_BUILD_OPTIONS}")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"../${CTEST_SOURCE_DIRECTORY}\"")
+
+ctest_start("Nightly")
+ctest_configure()
+ctest_build()
+ctest_test()
+ctest_submit()
