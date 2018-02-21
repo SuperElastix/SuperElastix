271692ef0cc89b3c7e8fd04893f34ba0100aa24b
Kasper Marstal <kaspermarstal@gmail.com>
2018-02-21 16:15:58 +0100

    BUG: Type caused images to be loaded instead of atlases

--- ContinuousRegistration/Source/datasets.py
+++ ContinuousRegistration/Source/datasets.py
@@ -61,7 +61,7 @@ class CUMC12(Dataset):
         image_file_names = [os.path.join(input_directory, 'Heads', image) for image in os.listdir(os.path.join(input_directory, 'Heads')) if image.endswith('.hdr')]
         self.image_file_names = [pair for pair in combinations(image_file_names, 2)]
 
-        atlas_file_names = [os.path.join(input_directory, 'Heads', atlas) for atlas in os.listdir(os.path.join(input_directory, 'Atlases')) if atlas.endswith('.hdr')]
+        atlas_file_names = [os.path.join(input_directory, 'Atlases', atlas) for atlas in os.listdir(os.path.join(input_directory, 'Atlases')) if atlas.endswith('.hdr')]
         self.atlas_file_names = [pair for pair in combinations(atlas_file_names, 2)]
 
         for image_file_name_0, image_file_name_1 in self.image_file_names:
@@ -222,7 +222,7 @@ class ISBR18(Dataset):
         image_file_names = [os.path.join(input_directory, 'Heads', image) for image in os.listdir(os.path.join(input_directory, 'Heads')) if image.endswith('.hdr')]
         self.image_file_names = [pair for pair in combinations(image_file_names, 2)]
 
-        atlas_file_names = [os.path.join(input_directory, 'Heads', atlas) for atlas in os.listdir(os.path.join(input_directory, 'Atlases')) if atlas.endswith('.hdr')]
+        atlas_file_names = [os.path.join(input_directory, 'Atlases', atlas) for atlas in os.listdir(os.path.join(input_directory, 'Atlases')) if atlas.endswith('.hdr')]
         self.atlas_file_names = [pair for pair in combinations(atlas_file_names, 2)]
 
         for image_file_name_0, image_file_name_1 in self.image_file_names:
@@ -306,14 +306,14 @@ class MGH10(Dataset):
 
         self.input_directory = input_directory
         self.image_file_names = []
-        self.atlas_file_names = []
+        self.label_file_names = []
         self.displacement_field_file_names = []
 
         image_file_names = [os.path.join(input_directory, 'Heads', image) for image in os.listdir(os.path.join(input_directory, 'Heads')) if image.endswith('.hdr')]
         self.image_file_names = [pair for pair in combinations(image_file_names, 2)]
 
-        atlas_file_names = [os.path.join(input_directory, 'Heads', atlas) for atlas in os.listdir(os.path.join(input_directory, 'Atlases')) if atlas.endswith('.hdr')]
-        self.atlas_file_names = [pair for pair in combinations(atlas_file_names, 2)]
+        atlas_file_names = [os.path.join(input_directory, 'Atlases', atlas) for atlas in os.listdir(os.path.join(input_directory, 'Atlases')) if atlas.endswith('.hdr')]
+        self.label_file_names = [pair for pair in combinations(atlas_file_names, 2)]
 
         for image_file_name_0, image_file_name_1 in self.image_file_names:
             image_file_name_0 = os.path.basename(image_file_name_0)
@@ -321,14 +321,14 @@ class MGH10(Dataset):
             image_file_name_we_0, image_extension_we_0 = os.path.splitext(image_file_name_0)
             image_file_name_we_1, image_extension_we_1 = os.path.splitext(image_file_name_1)
             self.displacement_field_file_names.append((os.path.join(self.name, image_file_name_we_0 + "_to_" + image_file_name_we_1 + ".nii"),
-                                                      os.path.join(self.name, image_file_name_we_1 + "_to_" + image_file_name_we_0 + ".nii")))
+                                                       os.path.join(self.name, image_file_name_we_1 + "_to_" + image_file_name_we_0 + ".nii")))
 
 
     def generator(self):
-        for image_file_names, atlas_file_names, displacement_field_file_names in zip(self.image_file_names, self.atlas_file_names, self.displacement_field_file_names):
+        for image_file_names, label_file_names, displacement_field_file_names in zip(self.image_file_names, self.label_file_names, self.displacement_field_file_names):
             yield {
                 "image_file_names": image_file_names,
-                "atlas_file_names": atlas_file_names,
+                "label_file_names": label_file_names,
                 "displacement_field_file_names": displacement_field_file_names
             }
 
@@ -336,7 +336,7 @@ class MGH10(Dataset):
     def evaluate(self, superelastix, file_names):
 
         singularity_ratio_0, singularity_ratio_1 = singularity_ratio(file_names['displacement_field_file_names_full_path'])
-        inverse_consistency_atlas_0, inverse_consistency_atlas_1 = inverse_consistency_labels(superelastix, file_names['atlas_file_names'], file_names['displacement_field_file_names_full_path'])
+        inverse_consistency_atlas_0, inverse_consistency_atlas_1 = inverse_consistency_labels(superelastix, file_names['label_file_names'], file_names['displacement_field_file_names_full_path'])
 
         result_0 = merge_dicts(singularity_ratio_0, inverse_consistency_atlas_0)
         result_1 = merge_dicts(singularity_ratio_1, inverse_consistency_atlas_1)
