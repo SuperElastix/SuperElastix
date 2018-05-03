0966f5b936deada9d707a5ac740088b84d4a1c2f
Jiri Borovec <jiri.borovec@fel.cvut.cz>
2018-05-03 16:19:15 +0200

    change nosetests

--- ContinuousRegistration/Source/test.py
+++ ContinuousRegistration/Source/test.py
@@ -1,4 +1,4 @@
-import unittest, os, tempfile
+import unittest, os, shutil
 from datetime import datetime
 import SimpleITK as sitk
 
@@ -6,8 +6,9 @@ from ContinuousRegistration.Source.metrics import tre
 
 
 def create_vtk_file(output_directory, point):
-    output_file_name = os.path.join(output_directory, "{:%Y-%m-%d-%H:%M:%S.%f}".format(datetime.now()) + ".vtk")
-    with open(output_file_name, 'w+') as f:
+    output_name = "{:%Y-%m-%d-%H:%M:%S.%f}".format(datetime.now()) + ".vtk"
+    output_path = os.path.join(output_directory, output_name)
+    with open(output_path, 'w+') as f:
         f.write("# vtk DataFile Version 2.0\n")
         f.write("SuperBench test point set\n")
         f.write("ASCII\n")
@@ -19,7 +20,7 @@ def create_vtk_file(output_directory, point):
 
         f.write("\n")
 
-    return output_file_name
+    return output_path
 
 
 def create_deformation_field_file(output_directory, deformation):
@@ -38,26 +39,40 @@ def create_deformation_field_file(output_directory, deformation):
 
 
 class TestEvaulationMetrics(unittest.TestCase):
+
+    TEMP_DIR = os.path.abspath('temporary-test-folder')
+
     def __init__(self, *args, **kwargs):
         # TODO: Pass via command line
         self.registration_driver = ""
         super(TestEvaulationMetrics, self).__init__(*args, **kwargs)
 
+        if not os.path.exists(self.TEMP_DIR):
+            os.mkdir(self.TEMP_DIR)
+
+    @unittest.skip("nonsense util `registration_driver` is given")
     def test_tre(self):
         point_0 = (10.0, 7.0, 6.0)
         point_1 = (9.0, 6.0, 8.0)
         deformation_0 = (-1., -1., 2.)
         deformation_1 = (1., 1., -2.)
-        with tempfile.TemporaryDirectory() as tmpdir:
-            point_set_0 = create_vtk_file(tmpdir, point_0)
-            point_set_1 = create_vtk_file(tmpdir, point_1)
-            deformation_field_0 = create_deformation_field_file(tmpdir, deformation_0)
-            deformation_field_1 = create_deformation_field_file(tmpdir, deformation_1)
-            tre_0, tre_1 = tre(self.registration_driver, (point_set_0, point_set_1),
-                               (deformation_field_0, deformation_field_1))
+
+        point_set_0 = create_vtk_file(self.TEMP_DIR, point_0)
+        point_set_1 = create_vtk_file(self.TEMP_DIR, point_1)
+        deformation_field_0 = create_deformation_field_file(self.TEMP_DIR, deformation_0)
+        deformation_field_1 = create_deformation_field_file(self.TEMP_DIR, deformation_1)
+        tre_0, tre_1 = tre(self.registration_driver, (point_set_0, point_set_1),
+                           (deformation_field_0, deformation_field_1))
 
         assert(tre_0['TRE'] == 0.0)
         assert(tre_1['TRE'] == 0.0)
+
+    def test_create_vtk_file(self):
+        path_file = create_vtk_file(self.TEMP_DIR, [7, 42])
+        assert(os.path.isfile(path_file))
+
+    def __del__(self):
+        shutil.rmtree(self.TEMP_DIR, ignore_errors=True)
 
 
 if __name__ == '__main__':
