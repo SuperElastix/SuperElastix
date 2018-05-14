f196f6ed8b9b859569b2e7db1ff14b2d1b53ab5d
Niels Dekker <N.Dekker@lumc.nl>
2018-05-14 15:55:54 +0200

    WIP: Copied "selxItkBSplineTransform*Component.h*" from "selxItkGaussianExponentialDiffeomorphicTransform*Component.h*"

---
+++ Modules/Components/itkImageRegistrationMethodv4/include/selxItkBSplineTransformComponent.h
@@ -0,0 +1,92 @@
+/*=========================================================================
+ *
+ *  Copyright Leiden University Medical Center, Erasmus University Medical
+ *  Center and contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef selxItkGaussianExponentialDiffeomorphicTransformComponent_h
+#define selxItkGaussianExponentialDiffeomorphicTransformComponent_h
+
+#include "selxSuperElastixComponent.h"
+
+#include "selxItkRegistrationMethodv4Interfaces.h"
+#include "selxSinksAndSourcesInterfaces.h"
+
+#include "itkGaussianExponentialDiffeomorphicTransform.h"
+
+namespace selx
+{
+template< class InternalComputationValueType, int Dimensionality >
+class ItkGaussianExponentialDiffeomorphicTransformComponent :
+  public SuperElastixComponent<
+  Accepting< itkImageDomainFixedInterface< Dimensionality >>,
+  Providing< itkTransformInterface< InternalComputationValueType, Dimensionality >>
+  >
+{
+public:
+
+  /** Standard ITK typedefs. */
+  typedef ItkGaussianExponentialDiffeomorphicTransformComponent<
+    InternalComputationValueType, Dimensionality
+    >                                       Self;
+  typedef SuperElastixComponent<
+    Accepting< itkImageDomainFixedInterface< Dimensionality >>,
+    Providing< itkTransformInterface< InternalComputationValueType, Dimensionality >>
+    >                                       Superclass;
+  typedef std::shared_ptr< Self >       Pointer;
+  typedef std::shared_ptr< const Self > ConstPointer;
+
+  ItkGaussianExponentialDiffeomorphicTransformComponent( const std::string & name, LoggerImpl & logger );
+  virtual ~ItkGaussianExponentialDiffeomorphicTransformComponent();
+
+  /** Get types from interfaces */
+  using TransformType      = typename itkTransformInterface< InternalComputationValueType, Dimensionality >::TransformType;
+  using TransformPointer   = typename itkTransformInterface< InternalComputationValueType, Dimensionality >::TransformPointer;
+  using ItkImageDomainType = typename itkImageDomainFixedInterface< Dimensionality >::ItkImageDomainType;
+
+  using GaussianExponentialDiffeomorphicTransformType
+      = typename itk::GaussianExponentialDiffeomorphicTransform< InternalComputationValueType, Dimensionality >;
+
+  //Accepting Interfaces:
+  virtual int Accept( typename itkImageDomainFixedInterface< Dimensionality >::Pointer ) override;
+
+  //Providing Interfaces:
+  virtual TransformPointer GetItkTransform() override;
+
+  //BaseClass methods
+  virtual bool MeetsCriterion( const ComponentBase::CriterionType & criterion ) override;
+
+  //static const char * GetName() { return "ItkGaussianExponentialDiffeomorphicTransform"; } ;
+  static const char * GetDescription() { return "ItkGaussianExponentialDiffeomorphicTransform Component"; }
+
+private:
+
+  typename GaussianExponentialDiffeomorphicTransformType::Pointer m_Transform;
+  typename ItkImageDomainType::Pointer m_FixedImageDomain;
+
+protected:
+
+  // return the class name and the template arguments to uniquely identify this component.
+  static inline const std::map< std::string, std::string > TemplateProperties()
+  {
+    return { { keys::NameOfClass, "ItkGaussianExponentialDiffeomorphicTransformComponent" }, { keys::InternalComputationValueType, PodString< InternalComputationValueType >::Get() }, { keys::Dimensionality, std::to_string( Dimensionality ) } };
+  }
+};
+} //end namespace selx
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "selxItkGaussianExponentialDiffeomorphicTransformComponent.hxx"
+#endif
+#endif // #define selxItkGaussianExponentialDiffeomorphicTransformComponent_h
---
+++ Modules/Components/itkImageRegistrationMethodv4/include/selxItkBSplineTransformParametersAdaptorsContainerComponent.hxx
@@ -0,0 +1,121 @@
+/*=========================================================================
+ *
+ *  Copyright Leiden University Medical Center, Erasmus University Medical
+ *  Center and contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "selxItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent.h"
+
+namespace selx
+{
+template< int Dimensionality, class TransformInternalComputationValueType >
+ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent< Dimensionality,
+TransformInternalComputationValueType >::ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent( const std::string & name,
+  LoggerImpl & logger )
+  : Superclass( name, logger )
+{
+}
+
+
+template< int Dimensionality, class TransformInternalComputationValueType >
+ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent< Dimensionality,
+TransformInternalComputationValueType >::~ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent()
+{
+}
+
+
+template< int Dimensionality, class TransformInternalComputationValueType >
+int
+ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent< Dimensionality, TransformInternalComputationValueType >
+::Accept( typename itkImageDomainFixedInterface< Dimensionality >::Pointer component )
+{
+  auto fixedImageDomain = component->GetItkImageDomainFixed();
+
+  for( unsigned int level = 0; level < m_shrinkFactorsPerLevel.Size(); level++ )
+  {
+    // We use the shrink image filter to calculate the fixed parameters of the virtual
+    // domain at each level.  To speed up calculation and avoid unnecessary memory
+    // usage, we could calculate these fixed parameters directly.
+
+    typedef itk::Image< TransformInternalComputationValueType, Dimensionality > FixedImageType;
+
+    typename FixedImageType::Pointer fixedImage = FixedImageType::New();
+    fixedImage->CopyInformation( fixedImageDomain );
+    //fixedImage->Allocate();
+
+    typedef itk::ShrinkImageFilter< FixedImageType, FixedImageType > ShrinkFilterType;
+    typename ShrinkFilterType::Pointer shrinkFilter = ShrinkFilterType::New();
+    shrinkFilter->SetShrinkFactors( m_shrinkFactorsPerLevel[ level ] );
+    shrinkFilter->SetInput( fixedImage );
+    shrinkFilter->UpdateOutputInformation();
+
+    typename TransformParametersAdaptorType::Pointer transformAdaptor = TransformParametersAdaptorType::New();
+    transformAdaptor->SetRequiredSpacing( shrinkFilter->GetOutput()->GetSpacing() );
+    transformAdaptor->SetRequiredSize( shrinkFilter->GetOutput()->GetLargestPossibleRegion().GetSize() );
+    transformAdaptor->SetRequiredDirection( shrinkFilter->GetOutput()->GetDirection() );
+    transformAdaptor->SetRequiredOrigin( shrinkFilter->GetOutput()->GetOrigin() );
+
+    m_adaptors.push_back( transformAdaptor.GetPointer() ); // Implicit cast back to TransformParametersAdaptorBase<itk::Transform<...>>
+  }
+
+  return 0;
+}
+
+
+template< int Dimensionality, class TransformInternalComputationValueType >
+typename ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent< Dimensionality,
+TransformInternalComputationValueType >::TransformParametersAdaptorsContainerType
+ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent< Dimensionality, TransformInternalComputationValueType >
+::GetItkTransformParametersAdaptorsContainer()
+{
+  return this->m_adaptors;
+}
+
+
+template< int Dimensionality, class TransformInternalComputationValueType >
+bool
+ItkGaussianExponentialDiffeomorphicTransformParametersAdaptorsContainerComponent< Dimensionality, TransformInternalComputationValueType >
+::MeetsCriterion( const ComponentBase::CriterionType & criterion )
+{
+  bool hasUndefinedCriteria( false );
+  bool meetsCriteria( false );
+  auto status = CheckTemplateProperties( this->TemplateProperties(), criterion );
+  if( status == CriterionStatus::Satisfied )
+  {
+    return true;
+  }
+  else if( status == CriterionStatus::Failed )
+  {
+    return false;
+  } // else: CriterionStatus::Unknown
+  else if( criterion.first == "ShrinkFactorsPerLevel" ) //Supports this?
+  {
+    meetsCriteria = true;
+
+    const int NumberOfResolutions = criterion.second.size(); // maybe check with criterion "NumberOfResolutions"?
+    m_shrinkFactorsPerLevel.SetSize( NumberOfResolutions );
+
+    unsigned int resolutionIndex = 0;
+    for( auto const & criterionValue : criterion.second ) // auto&& preferred?
+    {
+      m_shrinkFactorsPerLevel[ resolutionIndex ] = std::stoi( criterionValue );
+      ++resolutionIndex;
+    }
+  }
+
+  return meetsCriteria;
+}
+} //end namespace selx
