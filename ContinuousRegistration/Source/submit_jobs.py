4adde379a4d81ffdc7d3a7ff56652dbccc933cd7
Kasper Marstal <kaspermarstal@gmail.com>
2018-09-11 10:30:33 +0200

    ENH: Try instead to request more memory on cluster

--- ContinuousRegistration/Source/submit_jobs.py
+++ ContinuousRegistration/Source/submit_jobs.py
@@ -11,7 +11,7 @@ def main():
         #jt.jobEnvironment = os.environ
         #jt.email = ""
         #jt.workingDirectory = os.path.abspath(os.curdir)
-        jt.nativeSpecification = "-pe BWA 4 -l h_vmem=1g -cwd -j Y -V"
+        jt.nativeSpecification = "-pe BWA 4 -l h_vmem=5g -cwd -j Y -V"
 
         for dirpath, dirnames, filenames in os.walk('.'):
             for filename in filenames:
