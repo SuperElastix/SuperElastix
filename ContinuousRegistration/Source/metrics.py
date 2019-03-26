BUG: Fix broadcasting and handle nans in computation of inverse consistency

ContinuousRegistration/Source/metrics.py
@@ -43,19 +43,23 @@ def inverse_consistency(superelastix, displacement_field_file_names, mask_file_n
         composed_1 = sitk.GetArrayFromImage(sitk.ReadImage(compose_displacement_fields(superelastix, displacement_field_file_names[1], displacement_field_file_names[0])))
     except Exception:
         return (
-            { '3. InverseConsistency': np.NaN },
-            { '3. InverseConsistency': np.NaN }
+            {'3. InverseConsistency': np.NaN},
+            {'3. InverseConsistency': np.NaN}
         )
 
     mask_0 = sitk.GetArrayFromImage(sitk.ReadImage(mask_file_names[0])) > 0
-    mask_0 = np.tile(mask_0, [composed_0.shape[-1]] + [1] * mask_0.ndim)
+    norm_0 = np.linalg.norm(composed_0, axis=-1)
+    norm_0 = norm_0[mask_0]
+    norm_0 = np.ma.array(norm_0, mask=np.isnan(norm_0))
 
     mask_1 = sitk.GetArrayFromImage(sitk.ReadImage(mask_file_names[1])) > 0
-    mask_1 = np.tile(mask_1, [composed_1.shape[-1] + [1] * mask_1.ndim])
+    norm_1 = np.linalg.norm(composed_1, axis=-1)
+    norm_1 = norm_1[mask_1]
+    norm_1 = np.ma.array(norm_1, mask=np.isnan(norm_1))
 
     return (
-        {'3. InverseConsistency': np.mean(np.linalg.norm(composed_0[mask_0], axis=-1))},
-        {'3. InverseConsistency': np.mean(np.linalg.norm(composed_1[mask_1], axis=-1))}
+        {'3. InverseConsistency': np.mean(norm_0)},
+        {'3. InverseConsistency': np.mean(norm_1)}
     )
 
 